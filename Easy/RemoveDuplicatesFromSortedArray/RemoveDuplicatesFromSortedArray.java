b3a9445c9ea55d93da8aca63a9901c9b495a5c59
Edwin <emunoz9@neiu.edu>
2025-01-04 11:53:30 -0600

    Analyzed optimal solution

--- Easy/RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.java
+++ Easy/RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.java
@@ -3,7 +3,8 @@ package RemoveDuplicatesFromSortedArray;
 public class RemoveDuplicatesFromSortedArray {
     public static void main(String[] args) {
         int[] nums = new int[] { 1, 1, 1, 2, 2 };
-        int k = RemoveDuplicates(nums);
+        // int k = RemoveDuplicates(nums);
+        int k = optimalRemoveDuplicates(nums);
 
         for (int i = 0; i < k; i++) {
             System.out.print(nums[i] + " ");
@@ -22,13 +23,24 @@ public class RemoveDuplicatesFromSortedArray {
                 if (i + ++k >= n)
                     isDone = true;
             }
-            if (!isDone) {
+            if (!isDone && i + k < n) {
                 nums[i] = nums[i + k];
                 cNum = nums[i];
             }
         }
 
         return n - k;
+    }
+
+    // online solution
+    public static int optimalRemoveDuplicates(int[] nums) {
+        int index = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] != nums[i]) {
+                nums[index++] = nums[i];
+            }
+        }
+        return index;
     }
 
 }
