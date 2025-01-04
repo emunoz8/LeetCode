29b84f1fdbe8a93923f531cf3499888fb8ddc317
Edwin <emunoz9@neiu.edu>
2025-01-03 23:11:45 -0600

    started Remove Duplicate

---
+++ Easy/RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,36 @@
+package RemoveDuplicatesFromSortedArray;
+
+public class RemoveDuplicatesFromSortedArray {
+    public static void main(String[] args) {
+        int[] nums = new int[] { 1, 2, 2 };
+        int k = RemoveDuplicates(nums);
+
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+
+    }
+
+    public static int RemoveDuplicates(int[] nums) {
+        int k = 0;
+        int cNum = nums[0];
+        int n = nums.length;
+        boolean isDone = false;
+
+        for (int i = 1; i < n && !isDone; i++) {
+            isDone = true;
+            while (i + k < n && cNum == nums[i + k]) {
+                isDone = false;
+                if (i + ++k >= n)
+                    isDone = true;
+            }
+            if (!isDone) {
+                nums[i] = nums[i + k];
+                cNum = nums[i];
+            }
+        }
+
+        return n - k;
+    }
+
+}
