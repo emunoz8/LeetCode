154b2a150aeef6dea4dc6a6a1d1e1fdea01fca99
Edwin <emunoz9@neiu.edu>
2025-01-04 18:24:53 -0600

    included optimal solution

--- Easy/FindTheIndexOfTheFirstOccuranceInAString/FindTheIndexOfTheFirstOccuranceInAString.java
+++ Easy/FindTheIndexOfTheFirstOccuranceInAString/FindTheIndexOfTheFirstOccuranceInAString.java
@@ -20,7 +20,8 @@ public class FindTheIndexOfTheFirstOccuranceInAString {
             try {
                 j = 0;
                 if (b[j] == a[i] && b[last] == a[i + last]) {
-                    while (j <= last && b[j++] == a[i++]);
+                    while (j <= last && b[j++] == a[i++])
+                        ;
 
                     if (j - 1 == last)
                         return i - j;
@@ -34,6 +35,14 @@ public class FindTheIndexOfTheFirstOccuranceInAString {
             i++;
 
         }
+
+        return -1;
+    }
+
+    public static int optimalStrStr(String haystack, String needle) {
+        for (int i = 0, j = needle.length(); j <= haystack.length(); i++)
+            if (haystack.substring(i, j).equals(needle))
+                return i;
 
         return -1;
     }
