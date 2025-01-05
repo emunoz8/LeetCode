facc252c90eefb63d4d5b93e9432814e69d98581
Edwin <emunoz9@neiu.edu>
2025-01-04 18:38:31 -0600

    Completed Problem

---
+++ Easy/LengthOfLastWord/LengthOfLastWord.java
@@ -0,0 +1,31 @@
+package LengthOfLastWord;
+
+public class LengthOfLastWord {
+    public static void main(String[] args) {
+
+        String sentence = "Hello World    ";
+
+        int n = lengthOfLastWord(sentence);
+
+        System.out.println(n);
+    }
+
+    public static int lengthOfLastWord(String s) {
+
+        int len = 0;
+        int i = s.length() - 1;
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        while (i >= 0) {
+            if (s.charAt(i) != ' ')
+                len++;
+            else
+                return len;
+            i--;
+        }
+
+        return len;
+    }
+}
