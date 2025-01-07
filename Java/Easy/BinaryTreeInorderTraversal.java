8993a377db365076bafe74c54b9c8d5a704a164b
Edwin <emunoz9@neiu.edu>
2025-01-07 01:56:08 -0600

    Completed Binary Tree inorder

---
+++ Java/Easy/BinaryTreeInorderTraversal.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class BinaryTreeInorderTraversal {
+
+    public static void main(String[] args) {
+        TreeNode c = new TreeNode(3, null, null);
+        TreeNode b = new TreeNode(2, c, null);
+        TreeNode a = new TreeNode(1, null, b);
+
+        List<Integer> ans = inorderTraversal(a);
+
+        for (Integer n : ans) {
+            System.out.print(n + " ");
+        }
+
+    }
+
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> inorder = new ArrayList<>();
+
+        if (root != null)
+            helper(root, inorder);
+
+        return inorder;
+    }
+
+    public static void helper(TreeNode root, List<Integer> inorder) {
+
+        if (root.left != null)
+            helper(root.left, inorder);
+
+        inorder.add(root.val);
+
+        if (root.right != null)
+            helper(root.right, inorder);
+
+    }
+
+}
