d38bac1edee547b7d779b804fee5139e9a2b1dfd
Edwin <emunoz9@neiu.edu>
2025-01-08 18:18:52 -0600

    Completed problem

---
+++ Java/Easy/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,10 @@
+public class MaximumDepthOfBinaryTree {
+    // got it first try :^)
+    public static int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+}
