00058baf56952d499a8dbddb540809e11f7816c9
Edwin <emunoz9@neiu.edu>
2025-01-11 08:55:19 -0600

    completed problem

--- Java/Easy/BalancedBinaryTree.java
+++ Java/Easy/BalancedBinaryTree.java
@@ -3,4 +3,23 @@ public class BalancedBinaryTree {
     public static void main(String[] args) {
 
     }
+
+    public static boolean isBalanced(TreeNode root) {
+
+        return helper(root) != -1;
+    }
+
+    public static int helper(TreeNode root) {
+        if (root == null)
+            return 0;
+        int left, right;
+
+        left = helper(root.left);
+        right = helper(root.right);
+
+        if (left == -1 || right == -1 || Math.abs(left - right) > 1)
+            return -1;
+
+        return Math.max(left, right) + 1;
+    }
 }
