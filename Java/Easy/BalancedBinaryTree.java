411f5235390e317d7909ee6ab25d428c40a27a8e
Edwin <emunoz9@neiu.edu>
2025-01-11 09:01:45 -0600

    Provided optimal method

--- Java/Easy/BalancedBinaryTree.java
+++ Java/Easy/BalancedBinaryTree.java
@@ -9,6 +9,30 @@ public class BalancedBinaryTree {
         return helper(root) != -1;
     }
 
+    // does not create unnnecessary variables, returns -1 as soon as is not
+    // balanced.
+    // DFS
+    public static int optimalHelper(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int left = optimalHelper(root.left);
+
+        if (left == -1)
+            return -1;
+
+        int right = optimalHelper(root.right);
+
+        if (right == -1)
+            return -1;
+
+        if (Math.abs(left - right) > 1)
+            return -1;
+
+        return Math.max(left, right) + 1;
+    }
+
+    // not as optimal
     public static int helper(TreeNode root) {
         if (root == null)
             return 0;
