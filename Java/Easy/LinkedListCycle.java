8c4bd59301423a8777326a6b70408a57cc211ffe
emunoz8 <emunoz9@neiu.edu>
2025-01-13 16:46:22 -0600

    Completed Problem

--- Java/Easy/LinkedListCycle.java
+++ Java/Easy/LinkedListCycle.java
@@ -6,21 +6,58 @@ public class LinkedListCycle {
 
     }
 
-    public static ListNode detectCycles(ListNode head) {
-        ArrayList<ListNode> list = new ArrayList<>();
-        list.add(head);
+    public boolean optimalHasCycle(ListNode head){
+        if( head==null|| head.next==null){
+            return false;
+        }
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null&&fast.next!=null){
+        fast=fast.next.next;
+        slow=slow.next;
+         if(fast==slow){
+            return true;
+        }
+        }
+       return false;
 
-        while (head != null) {
-            if (!list.contains(head))
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ArrayList<ListNode> list = new ArrayList<>();
+        if(head == null)
+            return false;
+
+        do{
+            if(!list.contains(head))
                 list.add(head);
             else
-                return head;
+                return true;
 
             head = head.next;
 
-        }
+        }while(head != null);
 
-        return list.getFirst();
+
+        return false;
+        
     }
+
+    // public static ListNode detectCycles(ListNode head) {
+    //     ArrayList<ListNode> list = new ArrayList<>();
+    //     list.add(head);
+
+    //     while (head != null) {
+    //         if (!list.contains(head))
+    //             list.add(head);
+    //         else
+    //             return true;
+
+    //         head = head.next;
+
+    //     }
+
+    //     return null;
+    // }
 
 }
