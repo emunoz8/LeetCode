5254505bda0b1d921fa0a0a03b953ea3871c860c
emunoz8 <emunoz9@neiu.edu>
2025-01-13 09:31:50 -0600

    Added optimal solution

--- Java/Easy/PascalsTriangle.java
+++ Java/Easy/PascalsTriangle.java
@@ -2,11 +2,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PascalsTriangle{
-    
-    public static void main(String[] args) {
+
+    //optimal solution************************************
+    public static List<Integer> generateRow(int n){
+        List<Integer> row = new ArrayList<>();
+        int num = 1;
+
+        row.add(1);
+
+
+        for(int i=1; i < n;i++){
+            num *= (n-i);
+            num /=i;
+            row.add(num);
+
+        }
+
+        return row;
         
+    }
+
+    public static List<List<Integer>> optimalGenrate(int numRows){
+        List<List<Integer>> rVal = new ArrayList<>();
+
+        for(int i =1; i <= numRows;i++)
+            rVal.add(generateRow(i));
+
+        return rVal;
+
 
     }
+    //******************************************************
+    
     //first attempt
     public static List<List<Integer>> generate(int numRows) {
         List<List<Integer>> returnVal = new ArrayList<>();
@@ -21,7 +48,6 @@ public class PascalsTriangle{
             current.add(0, 1);
             for(; j < row.size(); j++ )
                 current.add(j, row.get(j-1) + row.get(j));
-
             current.add(1);
             row = current;
             returnVal.add(row);
