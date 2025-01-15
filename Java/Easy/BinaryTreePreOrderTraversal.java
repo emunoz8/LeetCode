6634128b89990fe82cd15b13e0b75c795c20ee1b
emunoz8 <emunoz9@neiu.edu>
2025-01-14 21:06:57 -0600

    Wordked on problem

---
+++ Java/Easy/BinaryTreePreOrderTraversal.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePreOrderTraversal{
+
+
+
+    public static List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+
+
+        return list;
+    }
+
+}
