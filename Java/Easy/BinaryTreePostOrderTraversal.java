51fa3a3661472d9d5317e2090444e590ba7dabc7
Edwin <emunoz9@neiu.edu>
2025-01-15 10:59:06 -0600

    completed problem

--- Java/Easy/BinaryTreePostOrderTraversal.java
+++ Java/Easy/BinaryTreePostOrderTraversal.java
@@ -1,7 +1,7 @@
 import java.util.List;
 import java.util.ArrayList;
 
-public class BinarayTreePostOrderTraversal {
+public class BinaryTreePostOrderTraversal {
     //left, right, root
     public static List<Integer> postOrderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
