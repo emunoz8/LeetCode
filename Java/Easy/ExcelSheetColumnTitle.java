03178e4035d0809561160c5134112366116a98c7
Edwin <emunoz9@neiu.edu>
2025-01-18 17:41:16 -0600

    Completed problem

--- Java/Easy/ExcelSheetColumnTitle.java
+++ Java/Easy/ExcelSheetColumnTitle.java
@@ -7,22 +7,16 @@ public class ExcelSheetColumnTitle {
     public static String convertToTitle(int columnNumber) {
         StringBuilder build = new StringBuilder();
 
-        int n = 1, d, r;
-        int sum = columnNumber;
-
-        for (; sum > 0; n++)
-            sum -= Math.pow(26, n);
-
-        for (int i = 1; i < n; i++) {
+        while (columnNumber > 0) {
+            columnNumber--;
+            build.append((char) ((columnNumber) % 26 + 'A'));
+            columnNumber /= 26;
 
         }
-        System.out.println(n);
-        System.out.println(columnNumber / 26);
-        System.out.println(columnNumber % 26);
 
-        //System.out.println((char) ((columnNumber % 27) + 64));
+        build.reverse();
 
-        return "";
+        return build.toString();
     }
 
 }
