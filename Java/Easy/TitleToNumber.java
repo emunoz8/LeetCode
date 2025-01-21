2101406526a073dfbf9d001e2212182b3ca49631
Edwin <emunoz9@neiu.edu>
2025-01-20 21:58:58 -0600

    formatted

--- Java/Easy/TitleToNumber.java
+++ Java/Easy/TitleToNumber.java
@@ -11,9 +11,8 @@ public class TitleToNumber {
     public static int titleToNumber(String columnTitle) {
         int num = 0;
 
-        for (int i = columnTitle.length() - 1, j = 0; i >= 0; i--, j++) {
+        for (int i = columnTitle.length() - 1, j = 0; i >= 0; i--, j++)
             num += (columnTitle.charAt(i) - 'A' + 1) * Math.pow(26, j);
-        }
 
         return num;
     }
