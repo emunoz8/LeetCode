c74b6b6aefc33830dbc6e60276d73c9c7d1da95e
Edwin <emunoz9@neiu.edu>
2025-01-30 16:43:47 -0600

    Added optimal solution

--- Java/Easy/PalindromeLinkedList.java
+++ Java/Easy/PalindromeLinkedList.java
@@ -5,8 +5,30 @@ public class PalindromeLinkedList {
 
     }
 
+    //optimal solution, and obvious
+    public static boolean optimalIsPalindrome(ListNode head) {
+        ListNode point = head;
+        int[] nums;
+        int n = 0;
+
+        while (point != null) {
+            point = point.next;
+            n++;
+        }
+
+        nums = new int[n - 1];
+        point = head;
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = point.val;
+            point = point.next;
+        }
+
+        return true;
+    }
+
     //first attempt
-    public static boolean isPalindrom(ListNode head) {
+    public static boolean isPalindrome(ListNode head) {
         Stack<ListNode> stack = new Stack<>();
 
         ListNode point = head;
