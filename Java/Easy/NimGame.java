0630a698a689f8483d6fa7e745baa0c1158ca2e6
Edwin <emunoz9@neiu.edu>
2025-02-03 19:12:27 -0600

    completed problem

---
+++ Java/Easy/NimGame.java
@@ -0,0 +1,12 @@
+public class NimGame {
+
+    public static void main(String[] args) {
+
+    }
+
+    //white boarded this, took a while.
+    public static boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+
+}
